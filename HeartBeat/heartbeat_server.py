5a93a20bc37fefb56d40ba0534032011bea9a2c7
mohdihabibi <mhabibi2@ford.com>
2019-03-20 01:29:10 -0700

    Implemented heartbeat for health check using grpc for a server cluster

---
+++ HeartBeat/heartbeat_server.py
@@ -0,0 +1,47 @@
+from concurrent import futures
+import time
+import logging
+
+import grpc
+
+import heartbeat_pb2
+import heartbeat_pb2_grpc
+
+_ONE_DAY_IN_SECONDS = 60 * 60 * 24
+
+
+class Heartbeat(heartbeat_pb2_grpc.HearBeatServicer):
+
+    def getStatus(self, request, context):
+        print("Request recieved from client. Client's IP address is: {}".format(request.ip))
+        return heartbeat_pb2.HeartBeatResponse(
+            ip='2222',
+            live=True,
+            cpu_usage=0.8,
+            disk_space=0.7,
+            num_process=11,
+            num_thread=20,
+            idle=0.9,
+            tot_mem=1.5,
+            used_mem=0.9,
+            data_read_per_sec=100.0,
+            data_write_per_sec=200.0,
+            data_recieve_per_sec=20.0,
+            data_sent_per_sec=10.0
+        )
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    heartbeat_pb2_grpc.add_HearBeatServicer_to_server(Heartbeat(), server)
+    server.add_insecure_port('[::]:3000')
+    server.start()
+    try:
+        while True:
+            time.sleep(_ONE_DAY_IN_SECONDS)
+    except KeyboardInterrupt:
+        server.stop(0)
+
+
+if __name__ == '__main__':
+    logging.basicConfig()
+    serve()
